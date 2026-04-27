335ccd4713200a3401d0b25c4ff2da83aade89fb
kanghyunuk <hukang.dev@gmail.com>
2026-04-27 14:20:23 +0900

    JAVA 02

--- JAVA02/src/Ch02/C04StringMain.java
+++ JAVA02/src/Ch02/C04StringMain.java
@@ -22,6 +22,7 @@ public class C04StringMain {
 		System.out.println("str1 == str3 ? " + (str1.equals(str3)));
 		System.out.println("str2 == str4 ? " + (str2.equals(str4)));
 		
+		System.out.println("TEST");
 	}
 
 }
