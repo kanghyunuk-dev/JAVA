5586473fa98b5e20054e859ce97ee246160909b6
kanghyunuk <hukang.dev@gmail.com>
2026-05-11 17:55:54 +0900

    JAVA 03

---
+++ JAVA03/src/Ch09/C01Client.java
@@ -0,0 +1,23 @@
+package Ch09;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+
+public class C01Client {
+
+	public static void main(String[] args) throws IOException {
+		Socket server = new Socket("192.168.5.12", 7000);
+		
+		InputStream in = server.getInputStream();
+		DataInputStream din = new DataInputStream(in);
+		String recv = din.readUTF();
+		System.out.println("메세지 : " + recv);
+		
+		din.close();
+		in.close();
+		server.close();
+	}
+
+}
